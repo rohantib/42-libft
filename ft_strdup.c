4a783eccd8044f568fd1f78b6711f8c775ad4da0
Rohan Tibrewal <rohan.tibrewal@gmail.com>
2017-07-06 23:43:29 -0700

    Revise strdup to use strcpy and save code

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,14 +6,7 @@ char *ft_strdup(char *src)
     str_length = ft_strlen(src);
     char *dup_string;
     dup_string = malloc(sizeof(char) * (str_length + 1));
-    char *start;
-    start = dup_string;
-    while(*src)
-    {
-        *dup_string = *src;
-        src++;
-        dup_string++;
-    }
-    *dup_string = *src;
-    return start;
+    ft_strcpy(dup_string, src);
+    dup_string[str_length] = '\0';
+    return dup_string;
 }
